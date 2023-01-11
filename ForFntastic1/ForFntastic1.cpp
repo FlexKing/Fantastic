5d696743fe539ac062db0710deb6f9844d8ca573
FlexKing <moniggleb@gmail.com>
2023-01-11 21:36:14 +0300

    Задание 1

---
+++ ForFntastic1/ForFntastic1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	string str ="";
+	getline(cin, str);
+	if (str == "") {
+		cout << "Вы оставили строку пустой";
+		return 0;
+	}
+	string answer;
+	for (int i = 0; i < str.length(); i++) {
+		char sym = str[i];
+		if (count(str.begin(), str.end(), sym) > 1) {
+			answer.append(")");
+		}
+		else
+		answer.append("(");
+	}
+	cout << answer;
+	return 0;
+}
+
